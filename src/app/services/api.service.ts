250a76a6d4688c6710fd22746315016b1b84e5a1
jan samus <j.samus@quantumfrog.de>
2024-05-13 12:40:40 +0200

    removed basic url handling - ip adress needs to be set when starting the app for the first time

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from  '@angular/common/http';
 import { DataStorageService } from '../services/data-storage.service'
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  defaultUrl = "http://192.168.20.107:5000/"
-  basicUrl:any = "http://192.168.20.107:5000/"
+  // defaultUrl = "http://192.168.20.107:5000/"
+  // basicUrl:any = "http://192.168.20.107:5000/"
+  basicUrl:any = null;
   port:any = ":5000"
   settingsUrl:any = "get_user_config"
   setSettingsUrl:any = "update_user_config"
@@ -23,149 +25,186 @@ export class ApiService {
   playerIDUrl:any ="update_player_ids"
   getServiceStatusUrl:any = "get_service_status"
 
-//basicUrl:any = "http://192.168.20.158/getconfig"
-
   constructor(private http: HttpClient,    private dataStorageService: DataStorageService    ) {
     this.setCorrectIPFromSaved();
   }
 
-
-getTestData() {
+  getTestData() {
     return this.http.get(this.basicUrl);
   }
-setCorrectIPFromSaved(){
-  //console.log("set correct ip")
-  //localStorage.removeItem("ipChange");
-  this.dataStorageService
-  .getStoredData('ipadress')
-  .then((ipadress) => {
-    if (ipadress != null) {
-      //console.log("correct ip is "+ipadress)
-      localStorage.setItem("ipChange", "Success")
-      this.basicUrl = "http://"+ipadress+this.port+"/"
-    }else{
-      //console.log("set default ip")
 
-      localStorage.setItem("ipChange", "DefaultURLSet")
-      this.basicUrl = this.defaultUrl
-    }
-  })
-}
-
-sendPlayerID() {
-  let payload = {
-    playerID: localStorage.getItem("playerID"),
+  setCorrectIPFromSaved(){
+    //console.log("set correct ip")
+    //localStorage.removeItem("ipChange");
+    this.dataStorageService
+    .getStoredData('ipadress')
+    .then((ipadress) => {
+      if (ipadress != null) {
+        localStorage.setItem("ipChange", "Success")
+        this.basicUrl = "http://"+ipadress+this.port+"/"
+      }else{
+        localStorage.setItem("ipChange", "DefaultURLSet")
+        this.basicUrl = null;
+      }
+    })
   }
-  //console.log("Player ID Sent " + playerID);
-  return this.http.post(this.basicUrl+this.playerIDUrl,payload)
+
+  sendPlayerID() {
+    let payload = {
+      playerID: localStorage.getItem("playerID"),
+    }
+    //console.log("Player ID Sent " + playerID);
+    return this.http.post(this.basicUrl+this.playerIDUrl,payload)
+      .subscribe(
+        data => console.log("Got data", data),
+        error => console.error("Got error", error)
+      );
+  }
+
+  getWaterLevel(interval:string="") {
+    var correctEndpoint = "waterlevelhistory.json"
+    switch (interval) {
+      case "today":
+        correctEndpoint = "waterlevelhistorydaily.json"
+        break;
+      case "daysofweek":
+          correctEndpoint = "waterlevelhistoryweekly.json"
+        break;
+      case "daysofmonth":
+          correctEndpoint = "waterlevelhistory.json"
+        break;
+      case "monthsofyear":
+            correctEndpoint = "waterlevelhistoryyearly.json"
+        break;
+
+      default:
+        // Code to be executed when timeframe does not match any case
+        //console.log("Invalid timeframe");
+        break;
+    }
+    return this.http.get(this.basicUrl+correctEndpoint);
+  }
+
+  getUserConfig(){
+    return this.http.get(this.basicUrl+this.settingsUrl);
+  }
+
+  async setUserConfig(payload): Promise<boolean> {
+    console.log("set user config: " + JSON.stringify(payload));
+    try {
+      const url = this.basicUrl + this.setSettingsUrl;
+      const response = await this.http.post(url, payload).toPromise();
+      console.log("Got data", response);
+      return true;  // Return true if the request was successful
+    } catch (error) {
+      console.error("Got error", error);
+      return false; // Return false if there was an error
+    }
+  }
+
+  getAutomationConfig(){
+    return this.http.get(this.basicUrl+this.automationUrl);
+  }
+
+  setAutomationConfig(payload){
+    //console.log("set automation config: "+JSON.stringify(payload))
+
+    return this.http.post(this.basicUrl+this.setAutomationUrl,payload)
+    .subscribe(
+      data => localStorage.setItem("message", JSON.stringify(data)),
+      //data => console.log("Got data", data),
+      error => console.error("Got error", error)
+    );
+  }
+
+  getDashboardConfig(){
+
+    this.setCorrectIPFromSaved();
+    //console.log("get dashboard config")
+
+    return this.http.get(this.basicUrl+this.dashboardConfigUrl);
+  }
+
+  setDashbardConfig(payload){
+    //console.log("setdashboard")
+    return this.http.post(this.basicUrl+this.dashboardSetConfigUrl,payload)
     .subscribe(
       data => console.log("Got data", data),
       error => console.error("Got error", error)
     );
-}
-
-getWaterLevel(interval:string="") {
-  var correctEndpoint = "waterlevelhistory.json"
-  switch (interval) {
-    case "today":
-      correctEndpoint = "waterlevelhistorydaily.json"
-      break;
-    case "daysofweek":
-        correctEndpoint = "waterlevelhistoryweekly.json"
-      break;
-    case "daysofmonth":
-        correctEndpoint = "waterlevelhistory.json"
-      break;
-    case "monthsofyear":
-          correctEndpoint = "waterlevelhistoryyearly.json"
-      break;
-
-    default:
-      // Code to be executed when timeframe does not match any case
-      //console.log("Invalid timeframe");
-      break;
   }
-  return this.http.get(this.basicUrl+correctEndpoint);
-}
 
-getUserConfig(){
+  drainComplete(payload){
+    //console.log("make drain request")
+    return this.http.get(this.basicUrl+this.drainUrl+"0.0")  .subscribe(
+      data => console.log("Got data", data),
+      error => console.error("Got error", error)
+    );
+  }
 
-  return this.http.get(this.basicUrl+this.settingsUrl);
+  drain(payload){
+    return this.http.get(this.basicUrl+this.drainUrl+payload)  .subscribe(
+      data => console.log("Got data", data),
+      error => console.error("Got error", error)
+    );
+  }
 
-}
-setUserConfig(payload){
-  //console.log("set user config: "+JSON.stringify(payload))
+  stopDrain(){
+    return this.http.get(this.basicUrl+this.stopDrainUrl)  .subscribe(
+      data => console.log("Got data", data),
+      error => console.error("Got error", error)
+    );
+  }
 
-  return this.http.post(this.basicUrl+this.setSettingsUrl,payload)
-  .subscribe(
-    data => console.log("Got data", data),
-    error => console.error("Got error", error)
-  );
+  customGetRequest(url){
+    return this.http.get(this.basicUrl+url)
+  }
 
-}
+  customPostRequest(url,payload){
+    return this.http.post(this.basicUrl+url,payload)  .subscribe(
+      data => console.log("Got data", data),
+      error => console.error("Got error", error)
+    );
+  }
 
-getAutomationConfig(){
-  return this.http.get(this.basicUrl+this.automationUrl);
-}
-setAutomationConfig(payload){
-  //console.log("set automation config: "+JSON.stringify(payload))
+  checkDeviceStatus(){
+    return this.http.get(this.basicUrl)
+  }
 
-  return this.http.post(this.basicUrl+this.setAutomationUrl,payload)
-  .subscribe(
-    data => localStorage.setItem("message", JSON.stringify(data)),
-    //data => console.log("Got data", data),
-    error => console.error("Got error", error)
-  );
-}
-getDashboardConfig(){
+  checkServiceStatus(){
+    return this.http.get(this.basicUrl+this.getServiceStatusUrl)
+  }
 
-  this.setCorrectIPFromSaved();
-  //console.log("get dashboard config")
+  checkControllerStatus(){
+    return this.http.get(this.basicUrl)
+  }
 
-  return this.http.get(this.basicUrl+this.dashboardConfigUrl);
-}
-setDashbardConfig(payload){
-  //console.log("setdashboard")
-  return this.http.post(this.basicUrl+this.dashboardSetConfigUrl,payload)
-  .subscribe(
-    data => console.log("Got data", data),
-    error => console.error("Got error", error)
-  );
-}
-drainComplete(payload){
-  //console.log("make drain request")
-  return this.http.get(this.basicUrl+this.drainUrl+"0.0")  .subscribe(
-    data => console.log("Got data", data),
-    error => console.error("Got error", error)
-  );
-}
-drain(payload){
-  return this.http.get(this.basicUrl+this.drainUrl+payload)  .subscribe(
-    data => console.log("Got data", data),
-    error => console.error("Got error", error)
-  );
-}
-stopDrain(){
-  return this.http.get(this.basicUrl+this.stopDrainUrl)  .subscribe(
-    data => console.log("Got data", data),
-    error => console.error("Got error", error)
-  );
-}
-customGetRequest(url){
-  return this.http.get(this.basicUrl+url)
-}
-customPostRequest(url,payload){
-  return this.http.post(this.basicUrl+url,payload)  .subscribe(
-    data => console.log("Got data", data),
-    error => console.error("Got error", error)
-  );
-}
+  getDashboardPrecipitationValues(): Observable<any[]> {
+    return this.getDashboardConfig().pipe(
+      map(data => {
+        // Check for null data or missing keys
+        if (!data || !data['forecast'] || data['current'] === undefined) {
+          throw new Error('Incomplete or missing data'); // Use throw to raise an error directly within map
+        }
 
-checkServiceStatus(){
-  return this.http.get(this.basicUrl+this.getServiceStatusUrl)
-}
+        let forecast = data['forecast'];
+        let fiveMinVal = data['current'];
+        let oneHourVal = this.summarizeForecastValues(11, forecast, fiveMinVal);
+        let twoHourVal = this.summarizeForecastValues(23, forecast, fiveMinVal);
 
-checkControllerStatus(){
-  return this.http.get(this.basicUrl)
-}
+        return [fiveMinVal, oneHourVal, twoHourVal];
+      }),
+      catchError(error => {
+        // Handle both the API fetch errors and errors thrown from map
+        console.error('Error fetching or processing data:', error);
+        return throwError(() => new Error('Error fetching precipitation values'));
+      })
+    );
+  }
+
+  private summarizeForecastValues(num: number, forecast: any, fiveMinVal: number): number {
+    let values: number[] = Object.values(forecast).slice(0, num).map(value => Number(value));
+    const sum: number = values.reduce((accumulator: number, currentValue: number) => accumulator + currentValue, 0);
+    return sum + fiveMinVal;
+  }
 }
