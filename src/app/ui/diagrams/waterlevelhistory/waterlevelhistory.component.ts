15d67f97b1e208bba1ef7091dbe9ed40de237ddf
jan samus <j.samus@quantumfrog.de>
2024-07-25 13:14:07 +0200

    chart updates get paused when app is paused/in background and will resume periodic updates once app is active/in foreground again formatted code

--- src/app/ui/diagrams/waterlevelhistory/waterlevelhistory.component.ts
+++ src/app/ui/diagrams/waterlevelhistory/waterlevelhistory.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core'
-import { ViewChild } from '@angular/core'
+import { Component, OnInit, Input } from '@angular/core';
+import { ViewChild } from '@angular/core';
 
-import { Chart } from 'chart.js/auto'
-import { DataStorageService } from '../../../services/data-storage.service'
-import { ApiService } from '../../../services/api.service'
-import {interval, Subscription} from "rxjs";
+import { Chart } from 'chart.js/auto';
+import { DataStorageService } from '../../../services/data-storage.service';
+import { ApiService } from '../../../services/api.service';
+import { interval, Subscription } from 'rxjs';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-waterlevelhistory',
@@ -12,115 +13,115 @@ import {interval, Subscription} from "rxjs";
   styleUrls: ['./waterlevelhistory.component.scss'],
 })
 export class WaterlevelhistoryComponent implements OnInit {
-  @ViewChild('barChart') barChart
+  @ViewChild('barChart') barChart;
 
   public checkConnection: Subscription;
+  private platformPauseSubscription: Subscription;
+  private platformResumeSubscription: Subscription;
 
-  bars: any
-  colorArray: any
+  bars: any;
+  colorArray: any;
   selectedTimeframeWaterlevel: any = {
     name: 'daysofweek',
     value: 'In den letzten 7 Tagen',
-  }
-  correctChartLabels: any = []
-  correctChartDataSet: any = []
+  };
+  correctChartLabels: any = [];
+  correctChartDataSet: any = [];
 
   timeframes = [
     { name: 'daysofweek', value: 'In den letzten 7 Tagen' },
     { name: 'daysofmonth', value: 'Im letzten Monat' },
     { name: 'monthsofyear', value: 'In den letzten 12 Monaten' },
     { name: 'today', value: 'Heute' },
-  ]
+  ];
   @Input() public typeOfDiagramm;
   @Input() public charttitle;
-  gridColor = 'rgba(255,255,255,0.6)'
+  gridColor = 'rgba(255,255,255,0.6)';
 
   constructor(
     private dataStorageService: DataStorageService,
     private apiService: ApiService,
-  ) {
+    private platform: Platform
+  ) {}
 
-  }
-
-  getDiagrammData(){
-    if(this.typeOfDiagramm=="waterlevel"){
+  getDiagrammData() {
+    if (this.typeOfDiagramm == 'waterlevel') {
     }
   }
-  createChart(){
+  createChart() {
     // console.log("create chart")
-    this.initialiseChartMetadata()
-    this.createLineChart()
+    this.initialiseChartMetadata();
+    this.createLineChart();
   }
 
-  parsedMaxVal:any;
-  maxValue:any=100;
-  stepSize:any=this.maxValue/5;
-  chartUrl:any="";
-  unitName:any="%";
-  datasetMultiplier:any=1000;
+  parsedMaxVal: any;
+  maxValue: any = 100;
+  stepSize: any = this.maxValue / 5;
+  chartUrl: any = '';
+  unitName: any = '%';
+  datasetMultiplier: any = 1000;
 
-  initialiseChartMetadata(){
+  initialiseChartMetadata() {
     switch (this.typeOfDiagramm) {
       case 'waterlevel':
-        this.chartUrl ="?column=waterlevel"
+        this.chartUrl = '?column=waterlevel';
         this.maxValue = 1000;
         this.stepSize = 200;
-        this.unitName=" L"
-        break
+        this.unitName = ' L';
+        break;
 
       case 'projectedPPT':
-        this.chartUrl ="?column=projectedPPT"
+        this.chartUrl = '?column=projectedPPT';
         this.maxValue = this.parsedMaxVal;
-        this.stepSize = this.parsedMaxVal/5;
-        this.datasetMultiplier=1;
-        this.unitName=" mm"
-        break
+        this.stepSize = this.parsedMaxVal / 5;
+        this.datasetMultiplier = 1;
+        this.unitName = ' mm';
+        break;
 
       case 'actualPPT':
-        this.chartUrl ="?column=actualPPT"
+        this.chartUrl = '?column=actualPPT';
         this.maxValue = this.parsedMaxVal;
-        this.stepSize = this.parsedMaxVal/5;
-        this.datasetMultiplier=1;
-        this.unitName=" mm"
-        break
+        this.stepSize = this.parsedMaxVal / 5;
+        this.datasetMultiplier = 1;
+        this.unitName = ' mm';
+        break;
 
       case 'overflow':
-        this.chartUrl ="?column=overflow"
+        this.chartUrl = '?column=overflow';
         this.maxValue = this.parsedMaxVal;
-        this.stepSize = this.parsedMaxVal/5;
-        this.unitName=" L"
-        break
+        this.stepSize = this.parsedMaxVal / 5;
+        this.unitName = ' L';
+        break;
 
       case 'stored':
-        this.chartUrl ="?column=stored"
+        this.chartUrl = '?column=stored';
         this.maxValue = this.parsedMaxVal;
-        this.stepSize = this.parsedMaxVal/5;
-        this.unitName=" L"
-        break
+        this.stepSize = this.parsedMaxVal / 5;
+        this.unitName = ' L';
+        break;
 
       case 'drained':
-        this.chartUrl ="?column=used"
+        this.chartUrl = '?column=used';
         this.maxValue = this.parsedMaxVal;
-        this.stepSize = this.parsedMaxVal/5;
-        this.unitName=" L"
-        break
+        this.stepSize = this.parsedMaxVal / 5;
+        this.unitName = ' L';
+        break;
 
       default:
-        this.chartUrl="get_daily_data"
-        break
+        this.chartUrl = 'get_daily_data';
+        break;
     }
   }
   createLineChart() {
-
-    const DISPLAY = true
-    const BORDER = true
-    const CHART_AREA = true
-    const TICKS = true
+    const DISPLAY = true;
+    const BORDER = true;
+    const CHART_AREA = true;
+    const TICKS = true;
     var context = this;
     this.bars = new Chart(this.barChart.nativeElement, {
       type: 'line',
       data: {
-        labels: [null,'MO', 'DI', 'MI', 'DO', 'FR', 'SA', 'SO'],
+        labels: [null, 'MO', 'DI', 'MI', 'DO', 'FR', 'SA', 'SO'],
         datasets: [
           {
             label: '',
@@ -148,159 +149,184 @@ export class WaterlevelhistoryComponent implements OnInit {
               display: DISPLAY,
               drawOnChartArea: CHART_AREA,
               drawTicks: TICKS,
-              color: this.gridColor
-            },    ticks: {
-              color:"white",
+              color: this.gridColor,
+            },
+            ticks: {
+              color: 'white',
               font: {
                 size: 14,
-            }},
+              },
+            },
           },
           y: {
             beginAtZero: true,
-            min:0,
-            max:this.maxValue,
+            min: 0,
+            max: this.maxValue,
             ticks: {
-              color:"white",
+              color: 'white',
               font: {
                 size: 14,
-            },
+              },
               stepSize: this.stepSize,
               callback: function (value, index, values) {
-                return value +context.unitName
+                return value + context.unitName;
               }.bind(context),
             },
             border: {
               display: false,
             },
             grid: {
-              color: this.gridColor
+              color: this.gridColor,
             },
           },
         },
       },
-    })
+    });
   }
   handleChange(e) {
-    this.selectedTimeframeWaterlevel = e.detail.value
+    this.selectedTimeframeWaterlevel = e.detail.value;
     this.dataStorageService.set(
-      'selectedtimeframewaterlevel'+this.typeOfDiagramm,
-      this.selectedTimeframeWaterlevel.name,
-    )
+      'selectedtimeframewaterlevel' + this.typeOfDiagramm,
+      this.selectedTimeframeWaterlevel.name
+    );
     this.getData();
   }
   updateChart() {
-    var parsedData:any =[]
-    parsedData = this.correctChartDataSet.map((dataset) => Math.floor(dataset.average*this.datasetMultiplier))
-    this.correctChartLabels = this.correctChartDataSet.map((dataset) => dataset.label)
+    var parsedData: any = [];
+    parsedData = this.correctChartDataSet.map((dataset) =>
+      Math.floor(dataset.average * this.datasetMultiplier)
+    );
+    this.correctChartLabels = this.correctChartDataSet.map(
+      (dataset) => dataset.label
+    );
 
-
-  if(this.bars==undefined){
-    this.createChart()
-  }
-  var context = this;
-      this.bars.data = {
-        labels: this.correctChartLabels,
-        datasets: [
-          {
-            label: '',
-            data: parsedData,
-            backgroundColor: 'rgb(255, 199, 39)',
-            borderColor: 'rgb(255, 199, 39)',
-            borderWidth: 1,
-          },
-        ],
-        options: {
-          scales: {
-            y: {
-              max:this.maxValue,
-                stepSize: this.stepSize,
-                callback: function (value, index, values) {
-                  return value +context.unitName
-                }.bind(context),
-              },
-            },
+    if (this.bars == undefined) {
+      this.createChart();
+    }
+    var context = this;
+    (this.bars.data = {
+      labels: this.correctChartLabels,
+      datasets: [
+        {
+          label: '',
+          data: parsedData,
+          backgroundColor: 'rgb(255, 199, 39)',
+          borderColor: 'rgb(255, 199, 39)',
+          borderWidth: 1,
+        },
+      ],
+      options: {
+        scales: {
+          y: {
+            max: this.maxValue,
+            stepSize: this.stepSize,
+            callback: function (value, index, values) {
+              return value + context.unitName;
+            }.bind(context),
           },
         },
-
-      this.bars.update()
-    }
-  getData(){
-    var endpointUrl = ""
+      },
+    }),
+      this.bars.update();
+  }
+  getData() {
+    var endpointUrl = '';
     switch (this.selectedTimeframeWaterlevel.name) {
       case 'daysofweek':
-        endpointUrl="get_weekly_data"
-        break
+        endpointUrl = 'get_weekly_data';
+        break;
       case 'daysofmonth':
-        endpointUrl="get_monthly_data"
+        endpointUrl = 'get_monthly_data';
 
-        break
+        break;
       case 'monthsofyear':
-        endpointUrl="get_yearly_data"
+        endpointUrl = 'get_yearly_data';
 
-        break
+        break;
       case 'today':
-        endpointUrl="get_daily_data"
+        endpointUrl = 'get_daily_data';
 
-        break
+        break;
       default:
-        endpointUrl="get_daily_data"
-        break
+        endpointUrl = 'get_daily_data';
+        break;
     }
 
-    this.apiService
-    .customGetRequest(endpointUrl+this.chartUrl)
-    .subscribe({
+    this.apiService.customGetRequest(endpointUrl + this.chartUrl).subscribe({
       next: (data) => {
         // console.log('response data getdiadata: ' + JSON.stringify(data))
-        this.correctChartDataSet = data
-        this.selectedTimeframeWaterlevel=this.selectedTimeframeWaterlevel[0]?this.selectedTimeframeWaterlevel[0]:this.selectedTimeframeWaterlevel
-        this.updateChart()
-        var parsedData:any =[]
-        parsedData = this.correctChartDataSet.map((dataset) => Math.floor(dataset.average*this.datasetMultiplier))
+        this.correctChartDataSet = data;
+        this.selectedTimeframeWaterlevel = this.selectedTimeframeWaterlevel[0]
+          ? this.selectedTimeframeWaterlevel[0]
+          : this.selectedTimeframeWaterlevel;
+        this.updateChart();
+        var parsedData: any = [];
+        parsedData = this.correctChartDataSet.map((dataset) =>
+          Math.floor(dataset.average * this.datasetMultiplier)
+        );
         // console.log("parsed data: "+JSON.stringify(parsedData))
-        this.setMaxValues(parsedData)
+        this.setMaxValues(parsedData);
       },
       error: (error) => {
-        console.log('Error HTTPResponse' + JSON.stringify(error))
+        console.log('Error HTTPResponse' + JSON.stringify(error));
       },
-    })
-
-
-
+    });
   }
   ngOnInit() {
     this.dataStorageService
-    .getStoredData('selectedtimeframewaterlevel'+this.typeOfDiagramm)
-    .then((savedTimeframeLevel) => {
-      if(savedTimeframeLevel!==null&&savedTimeframeLevel!=""){
-      this.selectedTimeframeWaterlevel = this.timeframes.filter(function (
-        data,
-      ) {
-        return data.name == savedTimeframeLevel
-      })
-    }
-      this.initialiseChartMetadata()
+      .getStoredData('selectedtimeframewaterlevel' + this.typeOfDiagramm)
+      .then((savedTimeframeLevel) => {
+        if (savedTimeframeLevel !== null && savedTimeframeLevel != '') {
+          this.selectedTimeframeWaterlevel = this.timeframes.filter(function (
+            data
+          ) {
+            return data.name == savedTimeframeLevel;
+          });
+        }
+        this.initialiseChartMetadata();
+        this.getData();
+        this.createChart();
+      });
+    this.checkConnection = interval(60000).subscribe(() => {
       this.getData();
-      this.createChart();
+    });
 
-    })
-    this.checkConnection = interval(60000).subscribe(x => {
-      this.updateChart();
+    this.platformResumeSubscription = this.platform.resume.subscribe(() => {
+      this.getData();
+      this.checkConnection = interval(60000).subscribe(() => {
+        this.getData();
+      });
+    });
+
+    this.platformPauseSubscription = this.platform.pause.subscribe(() => {
+      this.checkConnection.unsubscribe();
     });
   }
 
-  ionViewDidLeave(){
+  ngOnDestroy() {
+    if (this.checkConnection) {
+      this.checkConnection.unsubscribe();
+    }
+    if (this.platformResumeSubscription) {
+      this.platformResumeSubscription.unsubscribe();
+    }
+    if (this.platformPauseSubscription) {
+      this.platformPauseSubscription.unsubscribe();
+    }
+  }
+
+  ionViewDidLeave() {
     this.checkConnection.unsubscribe();
   }
-  
-  setMaxValues(data:[] ) {
+
+  setMaxValues(data: []) {
     // Ensure that the data array is not empty
     if (data.length === 0) {
-      throw new Error('Data array is empty.')
+      throw new Error('Data array is empty.');
     }
 
     // Find the maximum value in the array
-    const maxVal = Math.max(...data)
-    this.parsedMaxVal = maxVal + (maxVal/2)
+    const maxVal = Math.max(...data);
+    this.parsedMaxVal = maxVal + maxVal / 2;
   }
 }
